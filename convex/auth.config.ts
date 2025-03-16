e0a1a60d2853c8407f096e98b23af9646c0b69ca
ibambic <i.bambic@teamaxess.com>
2025-03-16 12:14:34 +0100

    #initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://huge-yeti-1.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ convex/schema.ts
@@ -0,0 +1,46 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    followers: v.number(),
+    following: v.number(),
+    posts: v.number(),
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"]),
+
+  posts: defineTable({
+    userId: v.id("users"),
+    imageUrl: v.string(),
+    storageId: v.id("_storage"),
+    caption: v.optional(v.string()),
+    likes: v.number(),
+    comments: v.number(),
+  }).index("by_user", ["userId"]),
+
+  likes: defineTable({
+    userId: v.id("users"),
+    postId: v.id("posts"),
+  })
+    .index("by_post", ["postId"])
+    .index("by_user_and_post", ["userId", "postId"]),
+
+  comments: defineTable({
+    userId: v.id("users"),
+    postId: v.id("posts"),
+    content: v.string(),
+  }).index("by_post", ["postId"]),
+
+  follows: defineTable({
+    followerId: v.id("users"),
+    followingId: v.id("users"),
+  })
+    .index("by_follower", ["followerId"])
+    .index("by_following", ["followingId"])
+    .index("by_both", ["followerId", "followingId"]),
+});
